510b2d974e0b4506f70b52cdb46496f67b40d41a
pipinalacraucki <143713787+pipinalacraucki@users.noreply.github.com>
2023-08-31 09:34:54 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,37 @@
+function cambiarColor() {
+    let tit = document.getElementById('titulo')
+    tit.style.color = 'purple'
+}
+
+function cambiarTamanoFuente() {
+    let tit = document.getElementById('titulo')
+    tit.style.fontSize = '60px'
+    
+}
+
+function cambiarColor() {
+    document.getElementById('1').style.color = '#ff0000'
+}
+
+function cambiarTamanoFuente() {
+    document.getElementById('1').style.fontSize = '10px'
+}
+
+function cambiarColor() {
+    document.getElementById('2').style.color = '#ff0000'
+}
+
+function cambiarTamanoFuente() {
+    document.getElementById('2').style.fontSize = '13px'
+}
+
+function cambiarColor() {
+    document.getElementById('3').style.color = '#ff0000'
+}
+
+function cambiarTamanoFuente() {
+    document.getElementById('3').style.fontSize = '20px'
+}
+
+
+
